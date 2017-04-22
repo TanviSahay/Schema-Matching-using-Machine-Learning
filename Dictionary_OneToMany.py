30b6b0fb16c350cbde6935de08e2c0baf3a4ff0e
Shruti Jadon <shrutijadon.0992@gmail.com>
2017-04-22 16:04:13 -0400

    Create Dictionary_OneToMany.py

---
+++ Dictionary_OneToMany.py
@@ -0,0 +1,12 @@
+Global_Dictionary={}
+Global_Dictionary[('Name')]=['First Name','Last Name','First_Name','Last_Name','fname','lname']
+Global_Dictionary[('Address','Location','Addr','Residence')]=['Street Name','S_Name','St_Name','St_No','Stree_Name','Apt Num','Apartment_Number','Apartment Number','Apartment No','Apt_Number']
+Global_Dictionary[('SUB-1_%')]=['S_SUB-1_%','*',0.01]
+Global_Dictionary[('TOB-1_%')]=['S_TOB-1_%','*',0.01]
+Global_Dictionary[('TOB-2_%')]=['S_TOB-2_%','*',0.01]
+Global_Dictionary[('TOB-2a_%')]=['S_TOB-2a_%','*',0.01]
+Global_Dictionary[('FUH-30_%')]=['S_FUH-30_%','*',0.01]
+Global_Dictionary[('FUH-7_%')]=['S_FUH-7_%','*',0.01]
+Global_Dictionary[('IMM-2_%')]=['S_IMM-2_%','*',0.01]
+
+print Global_Dictionary.keys()
